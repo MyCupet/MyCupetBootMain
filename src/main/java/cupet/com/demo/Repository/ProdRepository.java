4409df0b49a2ba0a4510dc3d2b69c6c251371216
jeongmin <jeongmin0521@naver.com>
2024-05-29 15:42:50 +0900

    쇼핑몰 페이지 수정

--- src/main/java/cupet/com/demo/Repository/ProdRepository.java
+++ src/main/java/cupet/com/demo/Repository/ProdRepository.java
@@ -1,8 +1,8 @@
 package cupet.com.demo.Repository;
 
-import cupet.com.demo.entity.cupetshopproductVO;
+import cupet.com.demo.entity.ShopVO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProdRepository extends JpaRepository<cupetshopproductVO, Integer>{
+public interface ProdRepository extends JpaRepository<ShopVO, Integer>{
 
 }
---
+++ src/main/java/cupet/com/demo/entity/ShopVO.java
@@ -0,0 +1,15 @@
+package cupet.com.demo.entity;
+
+
+import lombok.Data;
+
+@Data
+public class ShopVO {
+
+    private int cupet_prodno;
+    private String cupet_prodname;
+    private int cupet_prodprice;
+    private String cupet_prodcont;
+    private String cupet_prodimgpath;
+    private int cupet_proddiscountper;
+}
