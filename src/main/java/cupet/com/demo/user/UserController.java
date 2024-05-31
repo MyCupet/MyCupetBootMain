6e8f4b3a8b8deb424a053905c90854e855256b09
sinalol15 <pnsesh01@naver.com>
2024-05-31 10:31:17 +0900

    마이페이지토큰 가져와서보여주기

--- src/main/java/cupet/com/demo/user/UserController.java
+++ src/main/java/cupet/com/demo/user/UserController.java
@@ -18,23 +18,24 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api1")
 public class UserController {
 //   private final UserService userService;
-   private final AuthService authService;
+	private final AuthService authService;
 
-   @PostMapping("/userView")
-   @ResponseBody
-   public Map<String, Object> userView(@RequestHeader("Authorization") String token) {
-      System.out.println("사용자 상세보기 추출");
-      Map<String, Object> result = new HashMap<>();
-      try {
-         result = authService.AuthByUser(token);
-      } catch (MyCupetBootMainException e) {
-         result.put("error", e.getMessage());
-         result.put("status", false);
-         e.printStackTrace();
-      }
-//      UserVO user = userService.userView(String.valueOf(result.get("cupet_user_id")));
-//      result.put("user", user);
-//      result.put("status", true);
-      return result;
-   }
+	@PostMapping("/userView")
+	@ResponseBody
+	public Map<String, Object> userView(@RequestHeader("Authorization") String token) {
+		System.out.println("사용자 상세보기 추출");
+		Map<String, Object> result = new HashMap<>();
+		try {
+			// 토큰을 사용하여 사용자 정보 인증 및 추출
+			result = authService.AuthByUser(token);
+			// 상태 추가
+			result.put("status", true);
+		} catch (MyCupetBootMainException e) {
+			// 오류 처리
+			result.put("error", e.getMessage());
+			result.put("status", false);
+			e.printStackTrace();
+		}
+		return result;
+	}
 }
