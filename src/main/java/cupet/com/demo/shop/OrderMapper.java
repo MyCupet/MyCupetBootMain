a483bd7c8c780a24508e241c5dbd0feda9418618
jeongmin <jeongmin0521@naver.com>
2024-06-04 17:38:58 +0900

    주문페이지

---
+++ src/main/java/cupet/com/demo/shop/OrderMapper.java
@@ -0,0 +1,20 @@
+package cupet.com.demo.shop;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository("orderMapper")
+public interface OrderMapper {
+	
+	@Select("select * from cupetorder where cupet_user_id = #{cupet_user_id}")
+	List<OrderVO> findByUserId(String cupet_user_id);
+	
+	@Insert("INSERT INTO cupetorder (cupet_user_id, cupet_total_price, cupet_order_date, cupet_receiver_name, cupet_receiver_add, cupet_receiver_phone) "
+			+ "VALUES (#{cupet_user_id}, #{cupet_total_price}, #{cupet_order_date}, #{cupet_receiver_name}, #{cupet_receiver_add}, #{cupet_receiver_phone})")
+    @Options(keyProperty = "cupet_order_no")
+    int insert(OrderVO orderVO);
+	
+}
