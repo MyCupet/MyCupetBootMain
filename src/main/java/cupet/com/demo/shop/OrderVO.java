d30ffa11e310dac6df6bbecfdeaaba82147404ff
jeongmin <jeongmin0521@naver.com>
2024-06-05 15:35:19 +0900

    주문(결제x)

--- src/main/java/cupet/com/demo/shop/OrderVO.java
+++ src/main/java/cupet/com/demo/shop/OrderVO.java
@@ -6,9 +6,10 @@ import lombok.Data;
 public class OrderVO {
 	int cupet_order_no;
 	String cupet_user_id;
-	int cupet_total_price;
 	String cupet_receiver_name;
 	String cupet_receiver_add;
 	String cupet_receiver_phone;
+	int cupet_total_price;
+	String cupet_order_date;
 	int cupet_order_itemcnt;
 }
