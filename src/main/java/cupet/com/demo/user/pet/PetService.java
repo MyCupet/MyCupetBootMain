7f9ad686e3dbeb08cb870cbd5a9e902bfa0735a2
sinalol15 <pnsesh01@naver.com>
2024-06-08 01:11:29 +0900

    petInsert/Delete할때 cupet_pet_no 가져오도록 수정

--- src/main/java/cupet/com/demo/user/pet/PetService.java
+++ src/main/java/cupet/com/demo/user/pet/PetService.java
@@ -24,6 +24,11 @@ public class PetService {
     public PetVO petInsert(PetVO petVO) {
         try {
             petMapper.insertPet(petVO);
+            List<PetVO> petList = petView(petVO.getCupet_user_id());
+            if (!petList.isEmpty()) {
+                petVO.setCupet_pet_no(petList.get(petList.size() - 1).getCupet_pet_no());
+            }
+            System.out.println("펫정보"+petVO);
             return petVO;
         } catch (Exception e) {
             System.err.println("Error inserting pet: " + e.getMessage());
