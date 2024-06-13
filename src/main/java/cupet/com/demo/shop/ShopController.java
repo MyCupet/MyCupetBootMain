153f5b97254a3627bc130aa3887ff572945cfe70
jeongmin <jeongmin0521@naver.com>
2024-06-13 13:57:42 +0900

    쇼핑몰 상세보기

--- src/main/java/cupet/com/demo/shop/ShopController.java
+++ src/main/java/cupet/com/demo/shop/ShopController.java
@@ -1,11 +1,15 @@
 package cupet.com.demo.shop;
 
+import cupet.com.demo.MyCupetBootMainException;
 import cupet.com.demo.dto.PageRequestVO;
 import cupet.com.demo.dto.PageResponseVO;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +25,13 @@ public class ShopController {
     	return pageResponseVO;
     }
 	
+    
+    @GetMapping("/shop/{cupet_prodno}")
+    public ResponseEntity<ShopVO> shopDetail(@PathVariable("cupet_prodno") int cupet_prodno, @CookieValue(value = "token", required = false) String token) throws MyCupetBootMainException {
+
+        ShopVO prodinfo = shopService.findByProdNo2(cupet_prodno);
+        System.out.println("cupet_prodno = " + cupet_prodno);
+        System.out.println(prodinfo);
+        return new ResponseEntity<>(prodinfo, HttpStatus.OK);
+    }
 }
